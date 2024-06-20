9c3d65ef964ce4ba5a2af3c0a343473c201e7ef1
Eugen <androhit5566@gmail.com>
2024-06-20 18:23:14 +0300

    убрал имя

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println ("Привет");
         System.out.println ("Привет, всем");
+        System.out.println ("Привет");
+
     }
 }
